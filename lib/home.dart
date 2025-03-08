9f1d889bd62f779e01a8de240d2f7720b9d8b817
ayman <danishaiman3b@gmail.com>
2025-03-08 15:12:40 +0800

    added icons svg

--- lib/home.dart
+++ lib/home.dart
@@ -7,18 +7,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          )
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-      ),
+          title: Text('Breakfast',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          leading: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(10)),
+          )),
     );
   }
 }
