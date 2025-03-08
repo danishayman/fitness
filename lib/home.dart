320d5feac2919357ccf41d8d6cf2706fe0e73e22
ayman <danishaiman3b@gmail.com>
2025-03-08 18:34:14 +0800

    Fix formatting in main.dart and update binary configuration files

--- lib/home.dart
+++ lib/home.dart
@@ -7,16 +7,79 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Breakfast',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold)),
-          backgroundColor: Color(0xffF7F8F8),
-          elevation: 0.0,
-          centerTitle: true,
-          leading: Container(
+        appBar: appBar(),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            _searchField()
+          ],
+        ));
+  }
+
+  Container _searchField() {
+    return Container(
+            margin: EdgeInsets.only(left: 20, right: 20, top: 40),
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withAlpha(5),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+              ),
+            ]),
+            child: TextField(
+                decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: "Search Pancakes",
+              hintStyle: TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset("assets/icons/Search.svg"),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset("assets/icons/Filter.svg"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none),
+            )),
+          );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+        title: Text('Breakfast',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+        backgroundColor: Color(0xffF7F8F8),
+        elevation: 0.0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
             margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
                 color: Color(0xffF7F8F8),
@@ -27,7 +90,25 @@ class HomePage extends StatelessWidget {
               height: 20,
               width: 20,
             ),
-          )),
-    );
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              alignment: Alignment.center,
+              width: 37,
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+            ),
+          ),
+        ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: const HomePage()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: const HomePage());
   }
 }
