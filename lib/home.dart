e1374985136a345018ac546d1d4b1eeaed58418f
ayman <danishaiman3b@gmail.com>
2025-03-08 15:25:47 +0800

    Add SVG icon support and update HomePage styling

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,13 +13,20 @@ class HomePage extends StatelessWidget {
                   color: Colors.black,
                   fontSize: 18,
                   fontWeight: FontWeight.bold)),
-          backgroundColor: Colors.white,
+          backgroundColor: Color(0xffF7F8F8),
           elevation: 0.0,
           centerTitle: true,
           leading: Container(
             margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
-                color: Colors.black, borderRadius: BorderRadius.circular(10)),
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left 2.svg',
+              height: 20,
+              width: 20,
+            ),
           )),
     );
   }
